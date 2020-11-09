bd9815db772ef02f4a81af0d9df9d4115ebebb42
rrkrish561 <rrkrish561@gmail.com>
2020-11-09 08:39:47 -0500

    falling objects

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform enemy;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Transform>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        enemy.position += Vector3.down * speed;
+        if(enemy.position.y <= -6) {
+            Destroy(gameObject);
+        }
+    }
+}
