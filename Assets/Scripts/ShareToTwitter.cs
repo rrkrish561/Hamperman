827d391a1245ccd8b90b48441d52f671cab4aaba
rrkrish561 <rrkrish561@gmail.com>
2020-12-08 15:42:56 -0500

    opens twitter to share scores

---
+++ Assets/Scripts/ShareToTwitter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ShareToTwitter : MonoBehaviour
+{
+
+    private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+    private const string TWEET_LANGUAGE = "en";
+    public string descriptionParam;
+
+    public void ShareToTw() {
+        Application.OpenURL(TWITTER_ADDRESS + "?text=" + WWW.EscapeURL(descriptionParam));
+    }
+}
