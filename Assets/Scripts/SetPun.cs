8ad540328d41cf21f507c2e5b905ee47310996d4
rrkrish561 <rrkrish561@gmail.com>
2020-12-08 22:33:00 -0500

    Added puns

---
+++ Assets/Scripts/SetPun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPun : MonoBehaviour
+{
+    public Text txt;
+    public TextAsset dataFile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        string[] puns = dataFile.text.Split('\n');
+
+        int index = Random.Range(0, puns.Length);
+
+        txt.text = puns[index];
+    }
+}
