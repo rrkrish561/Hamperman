201265b37f77ba1fdceea1c62d23fdda3f6ad884
rrkrish561 <rrkrish561@gmail.com>
2020-12-09 18:07:19 -0500

    stuff

--- Backend/server/config/config.js
+++ Backend/server/config/config.js
@@ -1,7 +1,7 @@
 export default {
     db: {
       uri:
-        "mongodb://127.0.0.1:27017/HampermanHighScores",
+        "mongodb+srv://rradhak1:Krishna561@cluster0.ulgen.mongodb.net/HampermanHighScores?retryWrites=true&w=majority",
     },
     port: 5000,
   };
